6c1bc807df0d307e739ea22093a253f4abdbf037
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-02 00:21:34 +0600

    current time task is complete

--- script.js
+++ script.js
@@ -4,15 +4,54 @@ const cardNumberText = document.getElementById("card-number");
 let cardNumber = parseInt(cardNumberText.innerText);
 const totalNumberText = document.getElementById("total-number");
 let totalNumber = parseInt(totalNumberText.innerText);
+let historyCard = document.getElementById("card-add");
+const deleteButton = document.getElementById("delete-button");
 
 for (let i = 0; i < cardButton.length; i++) {
   cardButton[i].addEventListener("click", function () {
+    // Current Time
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    // Convert to 12-hour format
+    hours = hours % 12 || 12;
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
     const titles = cardTitle[i].innerText;
     const addedCard = document.getElementById("card-add");
     const createElement = document.createElement("p");
-    createElement.innerText = titles;
+    createElement.innerText =
+      "You have Completed the task" +
+      " " +
+      titles +
+      " " +
+      "at" +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds +
+      " " +
+      ampm;
+    createElement.classList.add(
+      "bg-[#F4F7FF]",
+      "font-bold",
+      "p-4",
+      "rounded-xl",
+      "mt-2"
+    );
     addedCard.appendChild(createElement);
-    alert("code successful");
+    alert("Task Updated Successful");
     cardButton[i].disabled = true;
 
     // dashboard card number decrement
@@ -20,26 +59,22 @@ for (let i = 0; i < cardButton.length; i++) {
     cardNumberText.innerText = cardNumber;
     console.log(cardNumber);
     //Total Card Number Decrement
-    totalNumber = totalNumber - 1;
+    totalNumber = totalNumber + 1;
     totalNumberText.innerText = totalNumber;
+
+    if (cardNumber === 0) {
+      alert("all card click");
+    }
   });
 }
-
-// const click = document.getElementById("handleClick");
-// click.addEventListener("click", function () {
-//   const cardTitle = document.getElementById("card-title").innerText;
-// const addedCard = document.getElementById("card-add");
-// const createElement = document.createElement("p");
-// createElement.innerText = cardTitle;
-// addedCard.appendChild(createElement);
-// document.getElementById("handleClick").disabled = true;
-
-// alert("code successful");
-// // });
+// Delete History
+deleteButton.addEventListener("click", function () {
+  alert("Delete All Successful ");
+  historyCard.innerText = "";
+});
 
 // Change Color
-
-function changeColor() {
+function changeBgColor() {
   const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
   document.getElementById("bgColor").style.backgroundColor = randomColor;
 }
